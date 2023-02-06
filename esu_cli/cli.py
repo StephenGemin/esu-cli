b24b9e33bbf5b704d0d2e55e9d6eb3396a69847f
Stephen Gemin <45926479+StephenGemin@users.noreply.github.com>
2023-02-06 16:10:32 -0500

    Add command to create pull request (#6)
    * add command to create pull request
    
    * fix logging

--- esu_cli/cli.py
+++ esu_cli/cli.py
@@ -1,16 +1,25 @@
+import logging
+
 import click
 import click_log
 
 from esu_cli import __version__
+from esu_cli.github import github_subcommands
+
+logger = logging.getLogger(__name__)
+click_log.basic_config(logger)
 
 
 @click.group()
-@click_log.simple_verbosity_option()
+@click_log.simple_verbosity_option(logger=logger, default="DEBUG", show_default=True)
 @click.version_option(__version__)
 def main():
     click.echo(f"ESU Command Line Interface version {__version__}")
     click.echo(err=True)
 
 
-if __name__ == '__main__':
+main.add_command(github_subcommands)
+
+
+if __name__ == "__main__":
     main()
---
+++ esu_cli/utils/click_wrappers.py
@@ -0,0 +1,17 @@
+import click
+
+
+class MessageWithCheckMark:
+    def __init__(self, message, ok="+", fail="x"):
+        self.message = message
+        self.ok = ok
+        self.fail = fail
+
+    def __enter__(self):
+        click.echo(self.message + " ", nl=False)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if all(v is None for v in [exc_type, exc_val, exc_tb]):
+            click.secho(self.ok, fg="green")
+        else:
+            click.secho(f"{self.fail} ({exc_val})", fg="red")
