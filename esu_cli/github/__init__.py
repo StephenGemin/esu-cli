0094f694d653da77b96d57b54e6984bc3df7df2f
Stephen Gemin <s.gemin88@gmail.com>
2023-02-17 23:17:26 -0500

    initial commit

--- esu_cli/github/__init__.py
+++ esu_cli/github/__init__.py
@@ -3,16 +3,16 @@ import logging
 import click
 import click_log
 
-from esu_cli.github import create_pull_request
+from esu_cli.github.pull_request import pr_subcommands
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 click_log.basic_config(logger)
 
 
-@click.group(name="github", chain=True)
+@click.group(name="github")
 def github_subcommands():
     """GitHub subcommands"""
 
 
-github_subcommands.add_command(create_pull_request.main)
+github_subcommands.add_command(pr_subcommands)
